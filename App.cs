7dd0d5a00abd54212626cd72e57d9eee197ae399
stofiiis <1222@student.itgymnazium.cz>
2024-12-06 10:50:26 +0100

    commit

---
+++ App.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGenerator
+{
+    internal class App
+    {
+        public char[] PasswordCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()".ToCharArray();
+
+        public void Generator()
+        {
+            Random random = new Random();
+            Console.WriteLine("Jak dlouhe chces heslo?");
+            int delka = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tvoje heslo je: ");
+            StringBuilder heslo = new StringBuilder();
+
+            for (int i = 0; i < delka; i++)
+            {
+                int index = random.Next(PasswordCharacters.Length);
+                heslo.Append(PasswordCharacters[index]);
+            }
+
+            Console.WriteLine(heslo.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PassGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.Generator();
+        }
+    }
+}
